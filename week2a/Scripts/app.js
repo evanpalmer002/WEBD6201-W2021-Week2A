debb31b5b9548e7bb157bdcb883c141f19ec9900
Evan <evan.palmer002@gmail.com>
2021-01-19 09:34:45 -0500

    added content

---
+++ week2a/Scripts/app.js
@@ -0,0 +1,15 @@
+/*custon javascript  goes here */
+
+// - Immediately invoked function Expression
+
+(function()
+{
+
+    function Start()
+    {
+        console.log("App Started...");
+
+    }
+
+    window.addEventListener("load", Start);
+})();
